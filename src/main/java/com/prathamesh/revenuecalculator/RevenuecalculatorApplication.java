8bdcf245ed51e46f217e94637ecc4fc3274f817a
Prathamesh Bhagat <prathameshbhagat0608@gmail.com>
2024-03-01 18:39:53 +0530

    build file changed to run the jar, Spaces in HTML, automatic browser loading,  added

--- src/main/java/com/prathamesh/revenuecalculator/RevenuecalculatorApplication.java
+++ src/main/java/com/prathamesh/revenuecalculator/RevenuecalculatorApplication.java
@@ -1,5 +1,10 @@
 package com.prathamesh.revenuecalculator;
  
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.awt.Desktop;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +12,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RevenuecalculatorApplication {
 
 	public static void main(String[] args) {
+		if (Desktop.isDesktopSupported()) {
+      
+			//making a desktop object
+			Desktop desktop = Desktop.getDesktop();
+			try {
+			   URI uri = new URI("http://localhost:8080/Home.html");
+			   desktop.browse(uri);
+			} catch (IOException excp) {
+			   excp.printStackTrace();
+			} catch (URISyntaxException excp) {
+			   excp.printStackTrace();
+			}
+		 }
 		SpringApplication.run(RevenuecalculatorApplication.class, args);
+		
 	}
 
 }
